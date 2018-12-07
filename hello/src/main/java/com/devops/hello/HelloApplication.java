14d970be01f43e7c16fe07f198ff7b89bef4b7c4
Tadeu Santos <tadeu.santos@accenture.com>
2018-12-06 22:21:36 -0300

    Spring setup

--- hello/src/main/java/com/devops/hello/HelloApplication.java
+++ hello/src/main/java/com/devops/hello/HelloApplication.java
@@ -2,8 +2,10 @@ package com.devops.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.devops.hello")
 public class HelloApplication {
 
 	public static void main(String[] args) {
